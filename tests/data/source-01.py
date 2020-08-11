97271c0b3abc94582183b4bd68b52cb6bffc8410
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-08-11 17:26:37 +0200

    Remove whitespace

--- tests/data/source-01.py
+++ tests/data/source-01.py
@@ -1,5 +1,3 @@
-
-
 def fib (limit) :
     a = 0
     b = 1
@@ -11,13 +9,11 @@ def fib (limit) :
         a,b = b,a
         b += a
 
-
 def list_fib(limit) :
     list = []
     generator = fib(limit)
     list = [i for i in generator]
     return list
-
 
 if __name__ == "__main__":
     assert list_fib(20) == [0, 1, 1, 2, 3, 5, 8, 13]
